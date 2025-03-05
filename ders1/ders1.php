8aee7cba8fb429c6161b64fd7f3cabcaadb15829
Ali Harman <aliharman02@gmail.com>
2025-03-05 12:33:00 +0300

    first commit

---
+++ ders1/ders1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dersleri</title>
+</head>
+
+<body>
+
+    <?php
+
+    //phpinfo()
+
+    echo"Aydın Adnan Menderes Üniveritesi Aydın MYO Bilgisayar Programcılığı";
+    echo"<br>Aydın Adnan Menderes Üniveritesi <br>Aydın MYO Bilgisayar Programcılığı <br>";
+    
+    
+        ?>
+
+</body>
+
+</html>
